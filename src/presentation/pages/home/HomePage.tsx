b20426d6287a13014dd5db7727e1c693965cbc77
Edinson Nuñez More <nmedinson@gmail.com>
2025-10-15 22:44:33 -0500

    Add HealthyUniversitySection component to HomePage and update imports

--- src/presentation/pages/home/HomePage.tsx
+++ src/presentation/pages/home/HomePage.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { HeroSection } from './components/HeroSection';
-import { WhatWeDoSection } from './components/WhatWeDoSection';
-import { VolunteerSection } from './components/VolunteerSection';
-import { NewsSection } from './components/NewsSection';
-import { AlliancesSection } from './components/AlliancesSection';
+import React from "react";
+import { HeroSection } from "./components/HeroSection";
+import { WhatWeDoSection } from "./components/WhatWeDoSection";
+import { HealthyUniversitySection } from "./components/HealthyUniversitySection";
+import { VolunteerSection } from "./components/VolunteerSection";
+import { NewsSection } from "./components/NewsSection";
+import { AlliancesSection } from "./components/AlliancesSection";
 
 export const HomePage: React.FC = () => {
   return (
     <div className="min-h-screen">
       <HeroSection />
       <WhatWeDoSection />
+      <HealthyUniversitySection />
       <VolunteerSection />
       <NewsSection />
       <AlliancesSection />
